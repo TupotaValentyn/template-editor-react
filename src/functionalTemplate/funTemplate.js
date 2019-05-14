8ed05179e1de4d25f15c3fcdbd94a0aed6602c1c
Valik_Tupota <gm.tupota.valik@gmail.com>
2019-05-14 12:29:22 +0300

    rewrite code to functional style for template component

---
+++ src/functionalTemplate/funTemplate.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react'
+
+
+export default function FunTemplate (mock) {
+
+  const {moc} = mock
+  // console.log(moc)
+  let [state, setState] = useState(0)
+
+  function count () {
+    internal.next()
+  }
+
+  function* contentRender () {
+    console.log(state)
+    yield setState(state + 1)
+  }
+
+  const internal = contentRender()
+  console.log()
+
+  return (
+      <section id="featured1" className="featured featured-1">
+      <h2>{state}</h2>
+      <h2></h2>
+      <h2></h2>
+      <h2></h2>
+      <button onClick={count}>push</button>     
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-4">
+            <div className="featured-panel">
+              <div className="featured-icon">
+                <i className="lnr lnr-screen"></i>
+              </div>
+              <div className="featured-content">
+                <h3 className="editable">Compatiblity Ready</h3>
+                <p className="editable">Duis sed odio sit amet nibh vuutate cursus a sit amet mauris morbi accumsan ipsum velit nam nec tellus viele a odio.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div className="col-xs-12 col-sm-12 col-md-4">
+            <div className="featured-panel">
+              <div className="featured-icon">
+                <i className="lnr lnr-star"></i>
+              </div>
+              <div className="featured-content">
+                <h3 className="editable">Up To Date</h3>
+                <p className="editable">Duis sed odio sit amet nibh vuutate cursus a sit amet mauris morbi accumsan ipsum velit nam nec tellus viele a odio.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div className="col-xs-12 col-sm-12 col-md-4">
+            <div className="featured-panel">
+              <div className="featured-icon">
+                <i className="lnr lnr-diamond"></i>
+              </div>
+              <div className="featured-content">
+                <h3 className="editable">Simple Design</h3>
+                <p className="editable">Duis sed odio sit amet nibh vuutate cursus a sit amet mauris morbi accumsan ipsum velit nam nec tellus viele a odio.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
